b76cb346ad5ba2d4e73a1853700715bf6deec5d8
Abhishek Bendre <prokyonz.tech@gmail.com>
2024-10-06 14:58:20 +0530

    stocks aded

---
+++ Accounts.Core/Controllers/StockController.cs
@@ -0,0 +1,55 @@
+﻿using Accounts.Core.Models;
+using Accounts.Core.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Accounts.Core.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StockController : ControllerBase
+    {
+        private readonly ILogger<StockController> _logger;
+        private readonly IStockRepository _stockRepository;
+
+        public StockController(ILogger<StockController> logger,
+            IStockRepository stockRepository)
+        {
+            _logger = logger;
+            _stockRepository = stockRepository;
+        }
+
+        [HttpGet]
+        public async Task<List<Stock>> Get()
+        {
+            var result = await _stockRepository.GetAllStock();
+            return result;
+        }
+
+        [HttpGet("GetStock")]
+        public async Task<Stock> GetRow(long stockId, int pageIndex, int pageSize)
+        {
+            var result = await _stockRepository.GetQuery(stockId, pageIndex, pageSize);
+            return result;
+        }
+
+        [HttpGet("GetStocksWithPagging")]
+        public async Task<List<Stock>> GetStocksWithPagging(int pageIndex, int pageSize)
+        {
+            var result = await _stockRepository.GetQuery(pageIndex, pageSize);
+            return result;
+        }
+
+        [HttpPost]
+        public async Task<Stock> Post(Stock stock)
+        {
+            try
+            {
+                return await _stockRepository.AddStockAsync(stock);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ Accounts.Core/Repositories/StockRepository.cs
@@ -0,0 +1,69 @@
+﻿using Accounts.Core.DbContext;
+using Accounts.Core.Models;
+using BaseClassLibrary.Interface;
+using System.Linq.Expressions;
+
+namespace Accounts.Core.Repositories
+{
+    public interface IStockRepository
+    {
+        Task<List<Stock>> GetAllStock();
+        Task<Stock> AddStockAsync(Stock customer);
+        Task<List<Stock>> GetQuery(int pageIndex, int pageSize);
+        Task<Stock> GetQuery(long stockId, int pageIndex, int pageSize);
+    }
+
+    public class StockRepository : IStockRepository
+    {
+        private readonly IBaseRepository<Stock, AppDbContext> _stockRepo;
+
+        public StockRepository(IBaseRepository<Stock, AppDbContext> stockRepo)
+        {
+            _stockRepo = stockRepo;
+        }
+
+        public async Task<Stock> AddStockAsync(Stock stock)
+        {
+            try
+            {
+                await _stockRepo.BeginTransactionAsync();
+
+                var result = await _stockRepo.AddAsync(stock);
+
+                await _stockRepo.CommitTransactionAsync();
+
+                return result;
+            }
+            catch (Exception)
+            {
+                await _stockRepo.RollbackTransactionAsync();
+                throw;
+            }
+        }
+
+        public async Task<List<Stock>> GetAllStock()
+        {
+            Expression<Func<Stock, bool>> predicate = c => c.Id > 0;
+
+            return await _stockRepo.GetAllAsync(predicate);
+        }
+
+        public async Task<List<Stock>> GetQuery(int pageIndex, int pageSize)
+        {
+            return await _stockRepo.QueryAsync(
+                query => query.Id > 0,
+                orderBy: c => c.Name ,
+                pageIndex, pageSize);
+        }
+
+        public async Task<Stock> GetQuery(long stockId, int pageIndex, int pageSize)
+        {
+            var result = await _stockRepo.QueryAsync(
+                query => query.Id == stockId,
+                orderBy: c => c.Name,
+                pageIndex, pageSize);
+
+            return result?.FirstOrDefault();
+        }
+    }
+}
