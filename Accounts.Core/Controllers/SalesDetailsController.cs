2215bbc7891a60ebfdb0726c4f73f0b1262d4d90
AbhishekBedre <abhishekbedre@gmail.com>
2024-11-17 15:37:58 +0530

    updated

---
+++ Accounts.Core/Controllers/SalesDetailsController.cs
@@ -0,0 +1,81 @@
+﻿using Accounts.Core.Models;
+using Accounts.Core.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Accounts.Core.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SalesDetailsController : ControllerBase
+    {
+        private readonly ILogger<SalesDetailsController> _logger;
+        private readonly ISalesDetailsRepository _salesDetailsRepository;
+
+        public SalesDetailsController(ILogger<SalesDetailsController> logger,
+            ISalesDetailsRepository salesDetailsRepository)
+        {
+            _logger = logger;
+            _salesDetailsRepository = salesDetailsRepository;
+        }
+
+        [HttpGet]
+        public async Task<List<SalesDetails>> Get()
+        {
+            var result = await _salesDetailsRepository.GetAllSalesDetails();
+            return result;
+        }
+
+        [HttpGet("GetSaleDetails")]
+        public async Task<SalesDetails> GetRow(long salesDetailsId, int pageIndex, int pageSize)
+        {
+            var result = await _salesDetailsRepository.GetQuery(salesDetailsId, pageIndex, pageSize);
+            return result;
+        }
+
+        [HttpGet("GetSalesDetailsWithPagging")]
+        public async Task<List<SalesDetails>> GetStocksWithPagging(int pageIndex, int pageSize)
+        {
+            var result = await _salesDetailsRepository.GetQuery(pageIndex, pageSize);
+            return result;
+        }
+
+        [HttpPost]
+        public async Task<SalesDetails> Post(SalesDetails salesDetails)
+        {
+            try
+            {
+                return await _salesDetailsRepository.AddSalesDetailsAsync(salesDetails);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        [HttpPut]
+        public async Task<SalesDetails> Put(SalesDetails salesDetails)
+        {
+            try
+            {
+                return await _salesDetailsRepository.UpdateSalesDetailsAsync(salesDetails);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        [HttpDelete]
+        public async Task<bool> Delete(long salesId)
+        {
+            try
+            {
+                return await _salesDetailsRepository.DeleteSalesDetailsAsync(salesId);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ Accounts.Core/Repositories/SalesDetailsRepository.cs
@@ -0,0 +1,83 @@
+﻿using Accounts.Core.DbContext;
+using Accounts.Core.Models;
+using BaseClassLibrary.Interface;
+using System.Linq.Expressions;
+
+namespace Accounts.Core.Repositories
+{
+    public interface ISalesDetailsRepository
+    {
+        Task<List<SalesDetails>> GetAllSalesDetails();
+        Task<SalesDetails> AddSalesDetailsAsync(SalesDetails salesDetails);
+        Task<SalesDetails> UpdateSalesDetailsAsync(SalesDetails salesDetails);
+        Task<bool> DeleteSalesDetailsAsync(long salesDetailsId);
+        Task<List<SalesDetails>> GetQuery(int pageIndex, int pageSize);
+        Task<SalesDetails> GetQuery(long stockDetailsId, int pageIndex, int pageSize);
+    }
+
+    public class SalesDetailsRepository : ISalesDetailsRepository
+    {
+        private readonly IBaseRepository<SalesDetails, AppDbContext> _salesDetailsRepo;
+
+        public SalesDetailsRepository(IBaseRepository<SalesDetails, AppDbContext> salesDetailsRepo)
+        {
+            _salesDetailsRepo = salesDetailsRepo;
+        }
+
+        public async Task<SalesDetails> AddSalesDetailsAsync(SalesDetails salesDetails)
+        {
+            try
+            {
+                await _salesDetailsRepo.BeginTransactionAsync();
+
+                var result = await _salesDetailsRepo.AddAsync(salesDetails);
+
+                await _salesDetailsRepo.CommitTransactionAsync();
+
+                return result;
+            }
+            catch (Exception)
+            {
+                await _salesDetailsRepo.RollbackTransactionAsync();
+                throw;
+            }
+        }
+
+        public async Task<bool> DeleteSalesDetailsAsync(long salesDetailsId)
+        {
+            await _salesDetailsRepo.DeleteAsync(salesDetailsId);
+            return true;
+        }
+
+        public async Task<List<SalesDetails>> GetAllSalesDetails()
+        {
+            Expression<Func<SalesDetails, bool>> predicate = c => c.Id > 0;
+
+            return await _salesDetailsRepo.GetAllAsync(predicate);
+        }
+
+        public async Task<List<SalesDetails>> GetQuery(int pageIndex, int pageSize)
+        {
+            return await _salesDetailsRepo.QueryAsync(
+                query => query.Id > 0,
+                orderBy: c => c.CreatedDate ?? DateTime.Now,
+                pageIndex, pageSize);
+        }
+
+        public async Task<SalesDetails> GetQuery(long stockDetailsId, int pageIndex, int pageSize)
+        {
+            var result = await _salesDetailsRepo.QueryAsync(
+               query => query.Id == stockDetailsId,
+               orderBy: c => c.CreatedDate,
+               pageIndex, pageSize);
+
+            return result?.FirstOrDefault();
+        }
+
+        public async Task<SalesDetails> UpdateSalesDetailsAsync(SalesDetails salesDetails)
+        {
+            await _salesDetailsRepo.UpdateAsync(salesDetails);
+            return salesDetails;
+        }
+    }
+}
