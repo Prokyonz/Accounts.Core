5f18e6f2e88386436c38bc9f37d9fe0f29459c7d
AbhishekBedre <abhishekbedre@gmail.com>
2024-11-23 15:45:54 +0530

    migration update

---
+++ Accounts.Core/Migrations/20241123101500_UserMasterColAdded.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Accounts.Core.Migrations
+{
+    public partial class UserMasterColAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AgentMaster");
+
+            migrationBuilder.AddColumn<string>(
+                name: "EmailId",
+                table: "UserMaster",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAgent",
+                table: "UserMaster",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<long>(
+                name: "ParentUserId",
+                table: "UserMaster",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EmailId",
+                table: "UserMaster");
+
+            migrationBuilder.DropColumn(
+                name: "IsAgent",
+                table: "UserMaster");
+
+            migrationBuilder.DropColumn(
+                name: "ParentUserId",
+                table: "UserMaster");
+
+            migrationBuilder.CreateTable(
+                name: "AgentMaster",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreatedBy = table.Column<long>(type: "bigint", nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    EmailId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MobileNo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UpdatedBy = table.Column<long>(type: "bigint", nullable: true),
+                    UpdatedDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    UserId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AgentMaster", x => x.Id);
+                });
+        }
+    }
+}
