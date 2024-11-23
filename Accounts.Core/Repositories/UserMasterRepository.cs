034f618a4bc6a09e2b72ef6d1be3f81355152c71
AbhishekBedre <abhishekbedre@gmail.com>
2024-11-23 15:39:03 +0530

    user master added

---
+++ Accounts.Core/Repositories/UserMasterRepository.cs
@@ -0,0 +1,86 @@
+using Accounts.Core.DbContext;
+using Accounts.Core.Models;
+using BaseClassLibrary.Interface;
+using System.Linq.Expressions;
+
+namespace Accounts.Core.Repositories
+{
+    public interface IUserMasterRepository
+    {
+        Task<List<UserMaster>> GetAllUserMasters();
+        Task<UserMaster> AddUserMasterAsync(UserMaster userMaster);
+        Task<UserMaster> UpdateUserMasterAsync(UserMaster userMaster);
+        Task<bool> DeleteUserMasterAsync(long userMasterId);
+        Task<List<UserMaster>> GetQuery(int pageIndex, int pageSize);
+        Task<UserMaster> GetQuery(long userMasterId, int pageIndex, int pageSize);
+    }
+}
+
+namespace Accounts.Core.Repositories
+{
+    public class UserMasterRepository : IUserMasterRepository
+    {
+        private readonly IBaseRepository<UserMaster, AppDbContext> _userMasterRepo;
+
+        public UserMasterRepository(IBaseRepository<UserMaster, AppDbContext> userMasterRepo)
+        {
+            _userMasterRepo = userMasterRepo;
+        }
+
+        public async Task<UserMaster> AddUserMasterAsync(UserMaster userMaster)
+        {
+            try
+            {
+                await _userMasterRepo.BeginTransactionAsync();
+
+                var result = await _userMasterRepo.AddAsync(userMaster);
+
+                await _userMasterRepo.CommitTransactionAsync();
+
+                return result;
+            }
+            catch (Exception)
+            {
+                await _userMasterRepo.RollbackTransactionAsync();
+                throw;
+            }
+        }
+
+        public async Task<bool> DeleteUserMasterAsync(long userMasterId)
+        {
+            await _userMasterRepo.DeleteAsync(userMasterId);
+            return true;
+        }
+
+        public async Task<List<UserMaster>> GetAllUserMasters()
+        {
+            Expression<Func<UserMaster, bool>> predicate = c => c.Id > 0;
+
+            return await _userMasterRepo.GetAllAsync(predicate);
+        }
+
+        public async Task<List<UserMaster>> GetQuery(int pageIndex, int pageSize)
+        {
+            return await _userMasterRepo.QueryAsync(
+                query => query.Id > 0,
+                orderBy: c => c.CreatedDate ?? DateTime.Now,
+                pageIndex, pageSize);
+        }
+
+        public async Task<UserMaster> GetQuery(long userMasterId, int pageIndex, int pageSize)
+        {
+            var result = await _userMasterRepo.QueryAsync(
+               query => query.Id == userMasterId,
+               orderBy: c => c.CreatedDate,
+               pageIndex, pageSize);
+
+            return result?.FirstOrDefault();
+        }
+
+        public async Task<UserMaster> UpdateUserMasterAsync(UserMaster userMaster)
+        {
+            await _userMasterRepo.UpdateAsync(userMaster);
+            return userMaster;
+        }
+    }
+}
