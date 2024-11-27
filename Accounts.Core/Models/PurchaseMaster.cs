2e94b1fad8c2d03fde3411e3e98a2e817570c069
admin <prokyonz.tech@gmail.com>
2024-11-27 17:58:11 +0530

    update purchase

--- Accounts.Core/Models/PurchaseMaster.cs
+++ Accounts.Core/Models/PurchaseMaster.cs
@@ -1,4 +1,5 @@
-﻿using BaseClassLibrary.Models;
+﻿using AutoMapper.Configuration.Annotations;
+using BaseClassLibrary.Models;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -13,9 +14,13 @@ namespace Accounts.Core.Models
         public long CustomerId { get; set; }
         public DateTime InvoiceDate { get; set; }
         public DateTime EntryDate { get; set; }
-        public string Description { get; set; }
+        public string? Description { get; set; }
         public long BrokerId { get; set; }
         public decimal DiscountAmount { get; set; }
+        public decimal BillAmount { get; set; }
+
+        [Ignore]
+        public virtual List<PurchaseDetails> PurchaseDetails { get; set; }
     }
 
     public class PurchaseDetails : BaseModel
@@ -25,6 +30,10 @@ namespace Accounts.Core.Models
         public long Id { get; set; }
         public long StockId { get; set; }
         public long PurchaseMasterId { get; set; }
-
+        public string? ItemDescription { get; set; }
+        public long Quantity { get; set; }
+        public decimal Rate { get; set; }
+        public decimal GSTAmount { get; set; }
+        public decimal Total { get; set; }
     }
 }
