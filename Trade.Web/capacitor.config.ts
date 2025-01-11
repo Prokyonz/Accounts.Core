5035c175d5706420d70afa15c5ec4f84a668a235
Anand Dhanani <anand.dhanani10@gmail.com>
2025-01-11 17:23:51 +0530

    Update App Name

--- Trade.Web/capacitor.config.ts
+++ Trade.Web/capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.prokyonz.app',
-  appName: 'Trade.Web',
+  appName: 'Sunsparkle',
   webDir: 'dist',
   bundledWebRuntime: false
 };
