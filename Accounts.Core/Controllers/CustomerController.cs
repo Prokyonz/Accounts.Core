8268d8301619ac0943d9513f7f6f8e86889faeff
admin <prokyonz.tech@gmail.com>
2025-02-19 17:53:33 +0530

    Added image data and removed the base64 from get all customer api

--- Accounts.Core/Controllers/CustomerController.cs
+++ Accounts.Core/Controllers/CustomerController.cs
@@ -11,6 +11,7 @@ using Accounts.Core.DbContext;
 using Microsoft.EntityFrameworkCore;
 using System.Drawing.Imaging;
 using Ghostscript.NET.Rasterizer;
+using System.Linq.Expressions;
 
 namespace Accounts.Core.Controllers
 {
@@ -30,10 +31,49 @@ namespace Accounts.Core.Controllers
             _appDbContext = appDbContext;
         }
 
+        [HttpGet("image-data")]
+        public async Task<List<Customer>> GetImageData(long id, bool isPan=false, bool isAdharFront=false, bool isAdharBack=false, bool isSign = false)
+        {
+            try
+            {
+                var result = await _appDbContext.CustomerMaster.AsQueryable().AsNoTracking().Where(x => x.IsDelete == false && x.Id == id).Select(x => new Customer
+                {
+                    Id = x.Id,
+                    IsDelete = x.IsDelete,
+                    PanImageData = isPan ? x.PanImageData : null,
+                    AadharImageFrontData = isAdharFront ? x.AadharImageFrontData : null,
+                    AadhbarImageBackData = isAdharBack ? x.AadhbarImageBackData : null,
+                    SignatureImageData = isSign ? x.SignatureImageData : null
+                }).ToListAsync();
+
+                return result;                     
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
         [HttpGet]
         public async Task<List<Customer>> Get()
         {
-            var result = await _customerMasterRepository.GetAllCustomers();
+            Expression<Func<Customer, bool>> predicate = c => c.Id > 0 && c.IsDelete == false;
+
+            var result = await _appDbContext.CustomerMaster.AsQueryable().AsNoTracking().Where(x => x.IsDelete == false).Select(x=> new Customer
+            {
+                Id  = x.Id,
+                FirstName = x.FirstName,
+                LastName =x.LastName,
+                Address =x.Address,
+                Pincode =x.Pincode,
+                MobileNo = x.MobileNo,
+                EmailId = x.EmailId,
+                AadharNo = x.AadharNo,
+                PanNo =x.PanNo,
+                IsDelete = x.IsDelete
+            }).ToListAsync();
+
+            //var result = await _customerMasterRepository.GetAllCustomers();
             return result;
         }
 
@@ -111,66 +151,66 @@ namespace Accounts.Core.Controllers
         {
             try
             {
-                var customerDt = ReadExcelFile();
+                await ReadExcelFile();
+                return;
+                //foreach (var item in customerDt?.Rows)
+                //{
+                //    DataRow row = ((System.Data.DataRow)item);
 
-                foreach (var item in customerDt?.Rows)
-                {
-                    DataRow row = ((System.Data.DataRow)item);
+                //    var customer = new Customer
+                //    {
+                //        FirstName = row.ItemArray[0].ToString(),
+                //        LastName = row.ItemArray[1].ToString(),
+                //        PanNo = row.ItemArray[2].ToString(),
+                //        AadharNo = row.ItemArray[3].ToString(),
+                //        Address = row.ItemArray[4].ToString(),
+                //        Pincode = long.Parse(row.ItemArray[5].ToString()),
+                //        PanImageData = row.ItemArray[6].ToString(),
+                //        AadharImageFrontData = row.ItemArray[7].ToString(),
+                //        AadhbarImageBackData = row.ItemArray[8].ToString(),
+                //        EmailId = "",
+                //        CreatedDate = DateTime.Now,
+                //        IsDelete = false,
+                //        MobileNo = "1234567890"
+                //        //SignatureImageData = row.ItemArray[6].ToString()
+                //    };
 
-                    var customer = new Customer
-                    {
-                        FirstName = row.ItemArray[0].ToString(),
-                        LastName = row.ItemArray[1].ToString(),
-                        PanNo = row.ItemArray[2].ToString(),
-                        AadharNo = row.ItemArray[3].ToString(),
-                        Address = row.ItemArray[4].ToString(),
-                        Pincode = long.Parse(row.ItemArray[5].ToString()),
-                        PanImageData = row.ItemArray[6].ToString(),
-                        AadharImageFrontData = row.ItemArray[7].ToString(),
-                        AadhbarImageBackData = row.ItemArray[8].ToString(),
-                        EmailId = "",
-                        CreatedDate = DateTime.Now,
-                        IsDelete = false,
-                        MobileNo = "1234567890",
-                        SignatureImageData = row.ItemArray[6].ToString()
-                    };
+                //    string query = @"
+                //        INSERT INTO CustomerMaster 
+                //        (FirstName, LastName, PanNo, AadharNo, Address, Pincode, [PanImageData], 
+                //         [AadharImageFrontData], [AadhbarImageBackData], EmailId, CreatedDate, 
+                //         IsDelete, MobileNo, [SignatureImageData])
+                //        VALUES 
+                //        (@FirstName, @LastName, @PanNo, @AadharNo, @Address, @Pincode, @PanImageData, 
+                //         @AadharImageFrontData, @AadharImageBackData, @EmailId, @CreatedDate, 
+                //         @IsDelete, @MobileNo, @SignatureImageData)";
 
-                    string query = @"
-                        INSERT INTO CustomerMaster 
-                        (FirstName, LastName, PanNo, AadharNo, Address, Pincode, [PanImageData], 
-                         [AadharImageFrontData], [AadhbarImageBackData], EmailId, CreatedDate, 
-                         IsDelete, MobileNo, [SignatureImageData])
-                        VALUES 
-                        (@FirstName, @LastName, @PanNo, @AadharNo, @Address, @Pincode, @PanImageData, 
-                         @AadharImageFrontData, @AadharImageBackData, @EmailId, @CreatedDate, 
-                         @IsDelete, @MobileNo, @SignatureImageData)";
+                //    using (SqlConnection sqlConnection = new SqlConnection(_appDbContext.Database.GetDbConnection().ConnectionString))
+                //    {
+                //        using (SqlCommand cmd = new SqlCommand(query, sqlConnection))
+                //        {
+                //            cmd.Parameters.AddWithValue("@FirstName", customer.FirstName);
+                //            cmd.Parameters.AddWithValue("@LastName", customer.LastName);
+                //            cmd.Parameters.AddWithValue("@PanNo", customer.PanNo);
+                //            cmd.Parameters.AddWithValue("@AadharNo", customer.AadharNo);
+                //            cmd.Parameters.AddWithValue("@Address", customer.Address);
+                //            cmd.Parameters.AddWithValue("@Pincode", customer.Pincode);
+                //            cmd.Parameters.AddWithValue("@PanImageData", customer.PanImageData);
+                //            cmd.Parameters.AddWithValue("@AadharImageFrontData", customer.AadharImageFrontData);
+                //            cmd.Parameters.AddWithValue("@AadharImageBackData", customer.AadhbarImageBackData);
+                //            cmd.Parameters.AddWithValue("@EmailId", customer.EmailId);
+                //            cmd.Parameters.AddWithValue("@CreatedDate", customer.CreatedDate);
+                //            cmd.Parameters.AddWithValue("@IsDelete", customer.IsDelete);
+                //            cmd.Parameters.AddWithValue("@MobileNo", customer.MobileNo);
+                //            cmd.Parameters.AddWithValue("@SignatureImageData", customer.SignatureImageData);
 
-                    using (SqlConnection sqlConnection = new SqlConnection(_appDbContext.Database.GetDbConnection().ConnectionString))
-                    {
-                        using (SqlCommand cmd = new SqlCommand(query, sqlConnection))
-                        {
-                            cmd.Parameters.AddWithValue("@FirstName", customer.FirstName);
-                            cmd.Parameters.AddWithValue("@LastName", customer.LastName);
-                            cmd.Parameters.AddWithValue("@PanNo", customer.PanNo);
-                            cmd.Parameters.AddWithValue("@AadharNo", customer.AadharNo);
-                            cmd.Parameters.AddWithValue("@Address", customer.Address);
-                            cmd.Parameters.AddWithValue("@Pincode", customer.Pincode);
-                            cmd.Parameters.AddWithValue("@PanImageData", customer.PanImageData);
-                            cmd.Parameters.AddWithValue("@AadharImageFrontData", customer.AadharImageFrontData);
-                            cmd.Parameters.AddWithValue("@AadharImageBackData", customer.AadhbarImageBackData);
-                            cmd.Parameters.AddWithValue("@EmailId", customer.EmailId);
-                            cmd.Parameters.AddWithValue("@CreatedDate", customer.CreatedDate);
-                            cmd.Parameters.AddWithValue("@IsDelete", customer.IsDelete);
-                            cmd.Parameters.AddWithValue("@MobileNo", customer.MobileNo);
-                            cmd.Parameters.AddWithValue("@SignatureImageData", customer.SignatureImageData);
+                //            await sqlConnection.OpenAsync();
+                //            int rowsAffected = await cmd.ExecuteNonQueryAsync();
 
-                            await sqlConnection.OpenAsync();
-                            int rowsAffected = await cmd.ExecuteNonQueryAsync();
-
-                        }
-                        //await _customerMasterRepository.AddCustomerAsync(customer);
-                    }
-                }
+                //        }
+                //        //await _customerMasterRepository.AddCustomerAsync(customer);
+                //    }
+                //}
             }
             catch (Exception ex)
             {
@@ -178,14 +218,14 @@ namespace Accounts.Core.Controllers
             }
         }
 
-        DataTable ReadExcelFile()
+        async Task ReadExcelFile()
         {
             ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
-            string filePath = "C:\\Users\\abhis\\Downloads\\Parth.xlsx";
+            string filePath = "D:\\Sunsparkle - Copy.xlsx";
 
             DataTable dt = new DataTable();
             dt.Columns.AddRange(new DataColumn[] {
-                new DataColumn("CustomerName", typeof(string)),
+                new DataColumn("FirstName", typeof(string)),
                 new DataColumn("LastName", typeof(string)),
                 new DataColumn("PanCardNo", typeof(string)),
                 new DataColumn("AdharCardNo", typeof(string)),
@@ -206,11 +246,11 @@ namespace Accounts.Core.Controllers
 
                     for (int row = 2; row <= rowCount; row++)
                     {
-                        if (!string.IsNullOrWhiteSpace(worksheet.Cells[row, 2].Text))
+                        if (!string.IsNullOrWhiteSpace(worksheet.Cells[row, 1].Text))
                         {
                             DataRow dr = dt.NewRow();
-                            dr["CustomerName"] = worksheet.Cells[row, 2].Text;
-                            //dr["LastName"] = worksheet.Cells[row, 2].Text;
+                            dr["FirstName"] = worksheet.Cells[row, 1].Text;
+                            dr["LastName"] = worksheet.Cells[row, 2].Text;
                             dr["PanCardNo"] = worksheet.Cells[row, 3].Text;
                             dr["AdharCardNo"] = worksheet.Cells[row, 4].Text;
                             dr["AdharAddress"] = worksheet.Cells[row, 5].Text;
@@ -223,20 +263,78 @@ namespace Accounts.Core.Controllers
 
                             var panBase64 = DownlaodFileFromGoodle(panUrl);
 
-                            Thread.Sleep(3000);
+                            //Thread.Sleep(3000);
 
                             var adaharFUrlBase64 = DownlaodFileFromGoodle(AdaharFUrl);
 
-                            Thread.Sleep(3000);
+                            //Thread.Sleep(3000);
 
                             var adaharBUrlBase64 = DownlaodFileFromGoodle(AdharBUrl);
+
+                            //Thread.Sleep(3000);
 
                             dr["PanCardPdfBase64"] = panBase64;
                             dr["AdharFrontPdfBase64"] = adaharFUrlBase64;
                             dr["AdharBackPdfBase64"] = adaharBUrlBase64;
-                            dr["UserId"] = 0; // worksheet.Cells[row, 10].Text
+                            dr["UserId"] = worksheet.Cells[row, 10].Text; // worksheet.Cells[row, 10].Text
 
                             dt.Rows.Add(dr);
+
+                            DataRow row1 = dr;
+
+                            var customer = new Customer
+                            {
+                                FirstName = row1.ItemArray[0].ToString(),
+                                LastName = row1.ItemArray[1].ToString(),
+                                PanNo = row1.ItemArray[2].ToString(),
+                                AadharNo = row1.ItemArray[3].ToString(),
+                                Address = row1.ItemArray[4].ToString(),
+                                Pincode = long.Parse(row1.ItemArray[5].ToString()),
+                                PanImageData = row1.ItemArray[6].ToString(),
+                                AadharImageFrontData = row1.ItemArray[7].ToString(),
+                                AadhbarImageBackData = row1.ItemArray[8].ToString(),
+                                EmailId = "",
+                                CreatedDate = DateTime.Now,
+                                IsDelete = false,
+                                MobileNo = "1234567890"
+                                //SignatureImageData = row.ItemArray[6].ToString()
+                            };
+
+                            string query = @"
+                        INSERT INTO CustomerMaster 
+                        (FirstName, LastName, PanNo, AadharNo, Address, Pincode, [PanImageData], 
+                         [AadharImageFrontData], [AadhbarImageBackData], EmailId, CreatedDate, 
+                         IsDelete, MobileNo)
+                        VALUES 
+                        (@FirstName, @LastName, @PanNo, @AadharNo, @Address, @Pincode, @PanImageData, 
+                         @AadharImageFrontData, @AadharImageBackData, @EmailId, @CreatedDate, 
+                         @IsDelete, @MobileNo)";
+
+                            using (SqlConnection sqlConnection = new SqlConnection(_appDbContext.Database.GetDbConnection().ConnectionString))
+                            {
+                                using (SqlCommand cmd = new SqlCommand(query, sqlConnection))
+                                {
+                                    cmd.Parameters.AddWithValue("@FirstName", customer.FirstName);
+                                    cmd.Parameters.AddWithValue("@LastName", customer.LastName);
+                                    cmd.Parameters.AddWithValue("@PanNo", customer.PanNo);
+                                    cmd.Parameters.AddWithValue("@AadharNo", customer.AadharNo);
+                                    cmd.Parameters.AddWithValue("@Address", customer.Address);
+                                    cmd.Parameters.AddWithValue("@Pincode", customer.Pincode);
+                                    cmd.Parameters.AddWithValue("@PanImageData", customer.PanImageData);
+                                    cmd.Parameters.AddWithValue("@AadharImageFrontData", customer.AadharImageFrontData);
+                                    cmd.Parameters.AddWithValue("@AadharImageBackData", customer.AadhbarImageBackData);
+                                    cmd.Parameters.AddWithValue("@EmailId", customer.EmailId);
+                                    cmd.Parameters.AddWithValue("@CreatedDate", customer.CreatedDate);
+                                    cmd.Parameters.AddWithValue("@IsDelete", customer.IsDelete);
+                                    cmd.Parameters.AddWithValue("@MobileNo", customer.MobileNo);
+                                    //cmd.Parameters.AddWithValue("@SignatureImageData", customer.SignatureImageData);
+
+                                    await sqlConnection.OpenAsync();
+                                    int rowsAffected = await cmd.ExecuteNonQueryAsync();
+
+                                }
+                                //await _customerMasterRepository.AddCustomerAsync(customer);
+                            }
                         }
                     }
                 }
@@ -246,7 +344,7 @@ namespace Accounts.Core.Controllers
                 throw new Exception("Excel file not found");
             }
 
-            return dt;
+            //return dt;
         }
 
         string DownlaodFileFromGoodle(string url)
@@ -262,10 +360,17 @@ namespace Accounts.Core.Controllers
                 UseShellExecute = true, // Ensures the URL is opened in the browser
             });
 
-            var list = new DirectoryInfo(@"C:\Users\abhis\Downloads").GetFiles().OrderByDescending(x => x.LastWriteTime).ToList().FirstOrDefault();
+            rtry:
+            var list = new DirectoryInfo(@"C:\Users\admin\Downloads").GetFiles().OrderByDescending(x => x.LastWriteTime).ToList().FirstOrDefault();
+            if(list == null)
+            {
+                Thread.Sleep(500);
+                goto rtry;
+            }
             string filePath = list?.FullName;
             string base64 = "";
 
+            Thread.Sleep(5000);
             if (list.FullName.ToLower().Contains("pdf"))
             {
                 using (var rasterizer = new GhostscriptRasterizer())
